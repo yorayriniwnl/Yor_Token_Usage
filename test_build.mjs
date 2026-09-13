4da92de2a812cc6d17e6deba13181c8c1a8de589
YorAyriniwnl <yorayriniwnl@gmail.com>
2026-09-14 01:57:05 +0530

    chore: remove temporary patch scripts

--- test_build.mjs
+++
@@ -1,8 +0,0 @@
-import { build } from 'esbuild';
-build({
-  entryPoints: ['src/measurement/accuracy-engine.ts'],
-  outdir: 'content',
-  format: 'iife',
-  bundle: true,
-  target: 'es2022'
-}).catch(() => process.exit(1));
